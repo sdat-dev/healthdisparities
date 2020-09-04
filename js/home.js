5f5dd1f69109c35a3577d2307818ce4cd1168345
sdat-dev <sdat@albany.eduq>
2020-09-04 12:39:23 -0400

    Added Carousel Options

--- js/home.js
+++ js/home.js
@@ -115,4 +115,6 @@ let addheader =  function (headers){
     header.innerHTML = content;
 }
 
-$('.carousel').carousel({pause: false});
+$('.carousel').carousel({pause: "false" ,  interval: 2000
+
+});
