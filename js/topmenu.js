9b630607caf12f54464f5a4e17e4f48b4c667918
sdat-dev <sdat@albany.edu>
2020-10-02 10:08:02 -0400

    auto commit due to data change

--- js/topmenu.js
+++ js/topmenu.js
@@ -2,7 +2,7 @@ let addTopMenu = function(){
     let navheader = document.getElementById('navbar-header');
     let headerContent = '<div class="ualbany-logo-wrapper">'+
                             '<div class = "display-flex">'+
-                                '<div class = "col-xl-4 col-lg-4 col-md-4 d-flex justify-content-center">' +
+                                '<div class = "col-xl-12 col-lg-12 col-md-12 d-flex justify-content-center">' +
                                     '<a href="https://www.suny.edu/">'+
                                         '<img class="sunny-logo" src="assets/images/SUNY-Logo.png" />'+
                                     '</a>'+
