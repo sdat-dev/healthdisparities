14159263ebe0f647f949d48bf54b9ef31ece59fd
sdat-dev <sdat@albany.edu>
2020-10-02 09:49:03 -0400

    auto commit due to data change

--- js/topmenu.js
+++ js/topmenu.js
@@ -2,21 +2,11 @@ let addTopMenu = function(){
     let navheader = document.getElementById('navbar-header');
     let headerContent = '<div class="ualbany-logo-wrapper">'+
                             '<div class = "display-flex">'+
-                                '<div class = "col-xl-4 col-lg-4 col-md-4 d-flex justify-content-start">' +
-                                    '<a href="https://www.albany.edu/">'+
-                                        '<img class="ualbany-logo" src="assets/images/logo.png" />'+
-                                    '</a>'+
-                                '</div>'+    
                                 '<div class = "col-xl-4 col-lg-4 col-md-4 d-flex justify-content-center">' +
                                     '<a href="https://www.suny.edu/">'+
                                         '<img class="sunny-logo" src="assets/images/SUNY-Logo.png" />'+
                                     '</a>'+
                                 '</div>'+  
-                                '<div class = "col-xl-4 col-lg-4 col-md-4 d-flex justify-content-end">' +
-                                    '<a href="https://www.downstate.edu/">'+
-                                        '<img class="downstate-logo" src="assets/images/SUNY-Downstate-Logo.png" />'+
-                                    '</a>'+
-                                '</div>'+ 
                             '</div>'+     
                         '</div>';
     navheader.innerHTML = headerContent;
